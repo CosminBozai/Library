04788d5b402cb2f6983684c9ff60dc77bc822756
CosminBozai <cosminbozai@outlook.com>
2022-08-12 11:48:43 +0100

    Function that adds a new book by pressing  button

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const table = document.getElementById("table");
+const addBtn = document.getElementById("add-book");
 
 let myLibrary = [];
 
@@ -10,28 +11,33 @@ function Book(author, title, year, pages, status) {
   this.status = status;
 }
 
-const a = new Book("Adsf", "dsfdsgf", "1920", "122", "Read");
-myLibrary.push(a);
-const b = new Book("dsgdfg", "dfdst", "2000", "134", "Not Read");
-myLibrary.push(b);
+addBtn.addEventListener("click", addNewBook);
 
-console.log(myLibrary);
+function addNewBook(author, title, year, pages, status) {
+  author = String(prompt("Author:"));
+  title = String(prompt("Title:"));
+  year = String(prompt("Year:"));
+  pages = String(prompt("Pages:"));
+  status = String(prompt("Status:"));
+  let x = new Book(author, title, year, pages, status);
+  myLibrary.push(x);
+  x.ID = myLibrary.indexOf(x);
+  displayBook();
+}
 
 function displayBook() {
-  for (i = 0; i < myLibrary.length; i++) {
-    let row = table.insertRow(-1);
-    let ID = row.insertCell(0);
-    let author = row.insertCell(1);
-    let title = row.insertCell(2);
-    let year = row.insertCell(3);
-    let pages = row.insertCell(4);
-    let status = row.insertCell(5);
+  let row = table.insertRow(-1);
+  let ID = row.insertCell(0);
+  let author = row.insertCell(1);
+  let title = row.insertCell(2);
+  let year = row.insertCell(3);
+  let pages = row.insertCell(4);
+  let status = row.insertCell(5);
 
-    ID.innerHTML = i;
-    author.innerHTML = myLibrary[i].author;
-    title.innerHTML = myLibrary[i].title;
-    year.innerHTML = myLibrary[i].year;
-    pages.innerHTML = myLibrary[i].pages;
-    status.innerHTML = myLibrary[i].status;
-  }
+  ID.innerHTML = myLibrary[myLibrary.length - 1].ID;
+  author.innerHTML = myLibrary[myLibrary.length - 1].author;
+  title.innerHTML = myLibrary[myLibrary.length - 1].title;
+  year.innerHTML = myLibrary[myLibrary.length - 1].year;
+  pages.innerHTML = myLibrary[myLibrary.length - 1].pages;
+  status.innerHTML = myLibrary[myLibrary.length - 1].status;
 }
